2038606b087aa8c08ebc436781c33e89465c98be
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-11 17:49:24 +0300

    Добавьте файлы проекта.

---
+++ lesson-5.6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number[3];
+
+    cout << "Какие числа вы хотите сравнить?\n";
+    cin >> number[0] >> number[1] >> number[2];
+
+    if (number[0] == number[1] && number[0] == number[2]) {
+        cout << "Число " << number[0] << " встречается 3 раза\n";
+    }
+    else if (number[0] == number[1] || number[0] == number[2]) {
+        cout << "Число " << number[0] << " встречается 2 раза\n";
+    }
+    else if (number[1] == number[2]) {
+        cout << "Число " << number[1] << " встречается 2 раза\n";
+    }
+    else {
+        cout << "Совпадений нет!\n";
+    }
+
+    return 0;
+}
